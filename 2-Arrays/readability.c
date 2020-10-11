3d7282c20d0a2f7406178d6f3139ecc73fb674ce
toni <antonia.hmaidi@uni-due.de>
2020-10-11 20:29:49 +0200

    Readability works as expected.

--- 2-Arrays/readability.c
+++ 2-Arrays/readability.c
@@ -1,33 +1,53 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main(int argc, string argv[])
 {
-    float words = argc;
+    string text = get_string("Text: ");
+    float words = 1;
     float letters = 0;
     float sentences = 0;
-    for (int i = 1; i < argc; i++)
+    for (int i = 0; i < strlen(text); i++)
     {
         // calculate average number of letters
-        for (int j = 0, n = strlen(argv[i]); j < n; j++)
-        {
-            if ((argv[i][j] >= 'a' && argv[i][j] <= 'z') ||
-            (argv[i][j] >= 'A' && argv[i][j] <= 'Z'))
+        if ((text[i] >= 'a' && text[i] <= 'z') ||
+            (text[i] >= 'A' && text[i] <= 'Z'))
             {
                 letters ++;
             }
-            if ((argv[i][j] == '.') || (argv[i][j] == '!') || (
-                argv[i][j] == '?')) 
-                {
-                    sentences ++;
-                }  
+        if ((text[i] == '.') || (text[i] == '!') || (
+                text[i] == '?')) 
+            {
+                sentences ++;
+            }  
+        if (text[i] == ' ')
+        {
+            words ++;
         }
     }
-    float L = letters/argc*100;
-    printf("%f", L);
-    float S = sentences/argc*100;
+    /*printf("Letters:");
+    printf("%f\n", letters);
+    printf("Words: ");
+    printf("%f\n", words);
+    printf("Sentences: ");
+    printf("%f\n", sentences);
+    */
+    float L = (letters/words)*100;
+    float S;
+    S = (sentences/words)*100;
     float readability;
     readability = 0.0588 * L - 0.296 * S - 15.8;
     printf("%f", readability);
+    if (readability < 1) {
+        printf("Before Grade 1");
+    }
+    else if (readability >= 16) {
+        printf("Grade 16+");
+    }
+    else {
+        int grade = round(readability);
+        printf("Grade %i", grade);
+    }
 }
